5483512ba7c917cc54a276d6403b95177e5ebccb
Yurii Karmazyn <70277985+Yurii-IvoryKing@users.noreply.github.com>
2022-08-02 21:28:10 +0200

    Delete js directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-(() => {
-    "use strict";
-    const imageSet = document.getElementById("imageInput");
-    const bgBlock = document.querySelector(".datepicker");
-    const setBtn = document.getElementById("setButton");
-    setBtn.addEventListener("click", (function(e) {
-        bgBlock.style.backgroundImage = `url(${imageSet.value})`;
-    }));
-})();
